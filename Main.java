e1e399ba09d225dfa18ff7c21f28cb0ce3430dda
hidoyet <102352895+hidoyet@users.noreply.github.com>
2022-11-10 12:18:28 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -7,18 +7,18 @@ public class Main {
 
 	public static void main(String[] args) {
 		Scanner scanner=new Scanner(System.in);
-		System.out.print("Dizi kaç elemanlý olacak?");
+		System.out.print("Dizi kaÃ§ elemanlÄ± olacak?");
 		int selection=scanner.nextInt();
 		int[] array=new int[selection];
 		
 		for(int i=0;i<array.length;i++) {
-			System.out.print(i+1+"'inci elemaný giriniz: ");
+			System.out.print(i+1+"'inci elemanÄ± giriniz: ");
 			array[i]=scanner.nextInt();
 			
 		}
 		
 		scanner.close();
-		System.out.print("Sýralama: ");
+		System.out.print("SÄ±ralama: ");
 		Arrays.sort(array);
 		for(int i=0;i<array.length;i++) {
 					System.out.print(array[i]+" ");
